4177d07ad1209068191a6b8603506fb330ebd01a
montekaka <josh.chen.jia@gmail.com>
2021-11-10 15:13:37 -0800

    adds seed file to create profile

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,3 +23,65 @@ profile = user.profiles.create({
   light_color: "#6E7598" # title color or #FFFFFF80
 })
 
+profile.social_networks.create([
+  {icon_name: 'twitter', name: 'Twitter', url: "https://twitter.com/settings/profile", sort_order: 1},
+  {icon_name: 'github', name: 'Github', url: "https://github.com/montekaka", sort_order: 2},  
+  {icon_name: 'dev', name: 'Dev.to', url: "https://dev.to/montekaka", sort_order: 3},  
+])
+
+profile.tech_skills.create([
+  {icon_name: 'react-js', name: 'React.js', sort_order: 1},
+  {icon_name: 'my-sql', name: 'MySQL', sort_order: 2},
+  {icon_name: 'nginx', name: 'Nginx', sort_order: 3},
+])
+
+profile.widgets.create([
+  {
+    is_dynamic_content: false, 
+    type: 'tweet',
+    link_type: "general",
+    section_name: "banner",
+    name: "Pinned Tweet", 
+    icon_name: "twitter",
+    post_title: "JustCast",
+    user_name: "thejustcast",    
+    avatar_url: "https://pbs.twimg.com/profile_images/1241559383475019776/YuSkJug9_400x400.jpg",
+    post_description: "Introducing Podcast Ninja: a free and open source fully customizable widget player that supports @PodcastindexOrg chapters namespace.",
+    url: "https://twitter.com/thejustcast/status/1427107126640398341"
+  }, 
+  {
+    is_dynamic_content: true, 
+    type: 'github_calendar',
+    link_type: "github",
+    section_name: "body",
+    name: "Github", 
+    icon_name: "github",
+    user_name: "montekaka",    
+    avatar_url: "https://pbs.twimg.com/profile_images/1241559383475019776/YuSkJug9_400x400.jpg",
+    url: "https://twitter.com/thejustcast/status/1427107126640398341",
+    sort_order: 1
+  },
+  {
+    is_dynamic_content: true, 
+    type: 'list',
+    section_name: "body",
+    name: "Medium Posts", 
+    icon_name: "medium",
+    user_name: "@justcastapp", 
+    link_type: 'medium',   
+    url: "https://medium.com/feed/@justcastapp",
+    sort_order: 2
+  },     
+  {
+    is_dynamic_content: true, 
+    type: "list",
+    section_name: "body",
+    name: "Dev.to Posts", 
+    icon_name: "dev",
+    user_name: "montekaka", 
+    link_type: 'dev',   
+    show_thumbnail: true,
+    url: "https://dev.to/feed/montekaka",
+    sort_order: 3
+  },       
+])
